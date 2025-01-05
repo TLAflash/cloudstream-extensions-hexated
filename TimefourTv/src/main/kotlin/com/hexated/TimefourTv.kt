ee8e5c3bc32188208ca20e0d9e54c4b4c5e27fe8
TLAflash <160544722+TLAflash@users.noreply.github.com>
2025-01-05 17:10:46 +0000

    Update TimefourTv.kt

--- TimefourTv/src/main/kotlin/com/hexated/TimefourTv.kt
+++ TimefourTv/src/main/kotlin/com/hexated/TimefourTv.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 class TimefourTv : MainAPI() {
-    override var mainUrl = "https://dlhd.sx"
+    override var mainUrl = "v3.sportsonline.ps"
     override var name = "Time4tv"
     override val hasDownloadSupport = false
     override val hasMainPage = true
